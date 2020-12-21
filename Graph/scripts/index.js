c42738d740b9eb912c2b12411963e4fd955e3762
Mayank Talwar <dreadarceus@gmail.com>
2020-12-21 21:32:39 +0530

    feat: quick sort on button press

--- Graph/scripts/index.js
+++ Graph/scripts/index.js
@@ -1,12 +1,6 @@
-const array = prompt("Enter an array:").split(" ");
+var array = prompt("Enter an array:").split(" ");
 for (var i = 0; i < array.length; i++) {
   array[i] = parseInt(array[i]);
-}
-var maxNum = 0;
-for (const x of array) {
-  if (x > maxNum) {
-    maxNum = x;
-  }
 }
 
 const body = document.querySelector("body");
@@ -30,7 +24,64 @@ for (var i = 0; i < array.length; i++) {
 }
 delete graph;
 
-const bars = document.getElementsByClassName("bar");
-for (var i = 0; i < bars.length; i++) {
-  bars[i].style.height = `${(maxH * array[i]) / maxNum}px`;
-}
+swap = (array, x, y) => {
+  const temp = array[x];
+  array[x] = array[y];
+  array[y] = temp;
+};
+
+quickSort = (array, start, end) => {
+  if (start >= end) {
+    return;
+  }
+  var pivot = Math.floor(Math.random() * (end - start + 1)) + start;
+  const pivot_value = array[pivot];
+  swap(array, pivot, end);
+  pivot = end;
+  var l = start,
+    r = end - 1;
+  while (true) {
+    while (l < end && array[l] < pivot_value) {
+      l++;
+    }
+    while (r >= start && array[r] >= pivot_value) {
+      r--;
+    }
+    pivot = r + 1;
+    if (l >= r) {
+      break;
+    }
+    swap(array, l, r);
+    pivot = r;
+  }
+  swap(array, pivot, end);
+  quickSort(array, start, pivot - 1);
+  quickSort(array, pivot + 1, end);
+  return array;
+};
+
+update_graph = (array) => {
+  const maxNum = max_element(array);
+  const bars = document.getElementsByClassName("bar");
+  for (var i = 0; i < bars.length; i++) {
+    bars[i].style.height = `${(maxH * array[i]) / maxNum}px`;
+  }
+};
+
+max_element = (array) => {
+  var maxNum = 0;
+  for (const x of array) {
+    if (x > maxNum) {
+      maxNum = x;
+    }
+  }
+  return maxNum;
+};
+
+update_graph(array);
+
+const start_btn = document.getElementById("start_btn");
+start_btn.addEventListener("click", () => {
+  array = quickSort(array, 0, array.length - 1);
+  update_graph(array);
+});
